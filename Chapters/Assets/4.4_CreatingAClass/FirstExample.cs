40900c69143d69e687ebaeabe294a1df9f33d7e4
CSharpWithUnity <learningcsharp@okita.io>
2018-06-05 19:35:19 -0700

    changing directory structure

---
+++ Chapters/Assets/4.4_CreatingAClass/FirstExample.cs
@@ -0,0 +1,10 @@
+﻿// Lets start here as you follow along
+
+public class MyNewClass
+{
+    //code goes here...
+}
+
+// the above is a complete class declaration.
+// the limes with the { and } contain the contents
+// of the class.
