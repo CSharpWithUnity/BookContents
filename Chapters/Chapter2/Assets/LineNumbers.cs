2641025d8def6ee62b53fca01ccaa5ce6f2954af
CSharpWithUnity <learningcsharp@okita.io>
2018-07-08 18:28:38 -0700

    adding LineNumbers example

---
+++ Chapters/Chapter2/Assets/LineNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineNumbers : MonoBehaviour
+{
+    public int SomeInt = 1;
+    public int OtherInt = 7;
+
+    // Use this for initialization
+    void Start()
+    {
+        print(MyFunction(SomeInt, OtherInt));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    int MyFunction(int a, int b)
+    {
+        return a + b;
+    }
+}
