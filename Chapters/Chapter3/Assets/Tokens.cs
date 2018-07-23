e11e455358d5cd077f4aaec35c421470a2801784
CSharpWithUnity <learningcsharp@okita.io>
2018-07-22 18:17:40 -0700

    merged some files into Tokens.cs

--- Chapters/Chapter3/Assets/Tokens.cs
+++ Chapters/Chapter3/Assets/Tokens.cs
@@ -3,6 +3,7 @@
 class Tokens
 {
     /*
+     * Section 3.3
      * ┌─────────────┐    ┌─────────────┐
      * │ punctuation │    │ punctuation │ 
      * └─────┬───────┘    └─────┬───────┘
@@ -29,6 +30,7 @@ class Tokens
     int i = 0;
 
     /*
+     * Section 3.3.1
      *    ┌────────────┐ ┌─────────┐
      *    │ identifier │ │ literal │ 
      *    └────┬───────┘ └──┬──────┘
@@ -51,13 +53,13 @@ class Tokens
 
     int j = 0; int k = 1;
     /*
+     * Section 3.3.2 Separator Tokens
+     * 
      * the above is perfectly valid, but
      * it's not as readable as:
      * int j = 0;
      * int k = 1;
-     */
-     
-    /*
+     * 
      * the l below us being assigned a 0 zero.
      * this works since the 0 is a literal of
      * type int, so this is a valid assignment
@@ -65,10 +67,14 @@ class Tokens
 
     int l = 0;
 
-    /* the line below is trying to assign the letter
+    /*
+     * Section 3.3.2 Separator Tokens Cont...
+     * 
+     * the line below is trying to assign the letter
      * O to the variable m which fails.
      * Uncomment the line to see what the error looks
      * like.
+     * 
      */
     //int m = O;
 
@@ -77,7 +83,10 @@ class Tokens
 
     }
 
-    /*    ┌──────────────────┐ ┌───────────────────┐
+    /*
+     * Section 3.3.2 Separator Tokens Cont...
+     * 
+     *    ┌──────────────────┐ ┌───────────────────┐
      *    │ Open Parenthesis │ │ Close Parenthesis │
      *    └───────────┬──────┘ └────┬──────────────┘
      *                │             │
@@ -102,7 +111,9 @@ class Tokens
     int[] arrayOfNumbers = { 1, (int)3.0, 9000 };
 
     /*
-     *  the above is another use of the curly braces
+     * Section 3.3.2 Separator Tokens Cont...
+     * 
+     * the above is another use of the curly braces
      * 
      *   ┌──────────────┐ ┌──────────────┐
      *   │ opening curly│ │ closing curly│
@@ -138,6 +149,25 @@ class Tokens
      *  the white space isn't necessary
      *  but it's easier to read.
      *  
+     *  
+     */
+    void QuotationMarks()
+    {
+        System.Console.Write(" use straight quotes. ");
+        // this line uses regular " quote marks
+
+        //System.Console.Write(“ this wont work... ”);
+        // the line above uses fancy quotes that most word processors will
+        // automatically insert when you use quotation marks.
+        // uncomment it if you'd like to see where the error occurs
+
+    }
+    /*
+     * Section 3.3.2 Separator tokens cont...
+     * " are usually used in text editors, smart quotes like
+     * the ones that are give in fancy word processors 
+     * like “ and ” aren't recognized here.
+     * 
      */
 
     void StatementSeparation()
@@ -150,19 +180,122 @@ class Tokens
             // int a = 1;
             System.Console.Write(a);
         }
+
+        /* code needs to be separated into small chunks
+         * to help separate code into usable parts we
+         * use separator tokens.
+         */
+        int a = 0;
+        /*
+         * the ; indicates the end of a statement.
+         */
+
+        int b = 1; int c = 2;
+        /*
+         * multiple statements can appear on the same line
+         * but it's not common practice to do so.
+         */
+
+        {
+            int d = 1;
+        }
+
+        /* 
+         * the curly braces isolate int d = 1; by separating
+         * it from the rest of the code in the Separators()
+         * function. more on this in a later chapter on scope
+         * in chapter 4.8
+         */
     }
 
-    void QuotationMarks()
+    /*
+     * Section 3.3.3 Operator Tokens
+     * 
+     * Operator tokens are often characters used in math operations like
+     * Addition Operator is + or - for Divide we use / rather than ÷
+     * ┌───┬───────────────────────┐
+     * │ = │ Assignment Operator   │
+     * ├───┼───────────────────────┤
+     * │ + │ Addition Operator     │
+     * ├───┼───────────────────────┤
+     * │ - │ Subtraction Operatorr │
+     * ├───┼───────────────────────┤
+     * │ * │ Multiply Operator     │
+     * ├───┼───────────────────────┤
+     * │ / │ Divide Operator       │
+     * ├───┼───────────────────────┤
+     * │ % │ Remainder Operator    │
+     * └───┴───────────────────────┘
+     * 
+     */
+    void OperatorTokens()
     {
-        System.Console.Write(" use straight quotes. ");
-        // this line uses regular " quote marks
+        int AdditionOperator = 1 + 1;
+        System.Console.WriteLine("AdditionOperatorResult = " + AdditionOperator);
 
-        //System.Console.Write(“ this wont work... ”);
-        // the line above uses fancy quotes that most word processors will
-        // automatically insert when you use quotation marks.
-        // uncomment it if you'd like to see where the error occurs
+        /*
+         * where are commas supposed to be used?
+         * they're used to keep values apart so
+         * they can be parsed as separate things
+         */
 
+        int a = 0, b = 1;
+        /* 
+         * the above statement creates two variables
+         * assigns them values, but only indicates
+         * the type once.
+         */
+
+        //int c = 0, int d = 1;
+        /*
+         * the above code fails
+         * the lexer isn't expecting
+         * the keyword int following the ,
+         * after the first assignment.
+         */
+
+        int e = 0; int f = 1;
+        /*
+         * the above code passes
+         * since the ; ends the first
+         * statement and the second
+         * declaration is expected
+         * since ; tells the lexer to
+         * read a new statement, not
+         * just a continuation of a previous
+         * statement.
+         */
+
+        int g = 0;
+        int h = 1;
+        /*
+         * the above passes as well since
+         * these are two independent statements,
+         * they just appear on two different lines.
+         */
     }
+
+    /*
+     * Section 3.3.5 Literals
+     * 
+     * values are literals
+     * numbers like 1 or 1.0 are both literals
+     * large values like 10,000 should be written
+     * as 10000 witout the comma.
+     * Remember we saw { 0, 1, 2 } which can also be
+     * {0,1,2} without the white space.
+     * 
+     * So how does the compiler know the difference between
+     * {10,000,1,2} and { 10,000, 1, 2} the compiler will see both
+     * as a 10 a 000 followed by 1 and finally a 2.
+     * 
+     * It's a bit hard to read really large numbers like 1234567891
+     * or 1,234,567,891. As humans we need to read these numbers
+     * with billions, millions, thousands, and hundreds, computers don't.
+     * 
+     */
+
+
 
     void imAFunction()
     {
