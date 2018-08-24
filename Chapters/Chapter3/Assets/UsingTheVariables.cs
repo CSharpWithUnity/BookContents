a6ea503c7d9204f3e5afd92b0ad9fb01a365326d
CSharpWithUnity <learningcsharp@okita.io>
2018-08-23 17:51:19 -0700

    added UsingTheVariables tutorial

---
+++ Chapters/Chapter3/Assets/UsingTheVariables.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingTheVariables : MonoBehaviour
+{
+    /*
+     * Section 3.10.3 Using The Variables
+     */
+
+    public float RotationSpeed;
+    public float Rotation;
+    public float ForwardSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+         * Section 3.10.3 continued...
+         * 
+         * Update the Rotation with RotationSpeed
+         * multiplied by Time.deltaTime.
+         */
+
+        Rotation += RotationSpeed * Time.deltaTime;
+
+        /*
+         * now add this to the objects Y rotation
+         * the second value in the object's local
+         * euler angles.
+         */
+
+        transform.localEulerAngles = new Vector3(0, Rotation, 0);
+
+        /*
+         * the transform.forward value is something provided
+         * by the Unity API.
+         */
+
+        transform.position += transform.forward * (ForwardSpeed * Time.deltaTime);
+	}
+}
