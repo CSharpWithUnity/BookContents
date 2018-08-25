fdd1c261e8032c6de7c4b4b16be23185d23a8fb7
CSharpWithUnity <learningcsharp@okita.io>
2018-08-25 03:32:16 -0700

    added section 4.8 comments and info

--- Chapters/Chapter4/Assets/Scope.cs
+++ Chapters/Chapter4/Assets/Scope.cs
@@ -16,18 +16,20 @@ public class Scope : MonoBehaviour
     /*
      * Section 4.8.1 Class Scope
      */
-    
+
     int myInt = 1;
     /* Class Scope
      * Visible to the entire class
      */
 
-	void Start ()
+    // float myInt = 1; //nope. can't redefine myInt
+
+    void Start()
     {
         /*
          * Section 4.8 Scope
          */
-        
+
         for (int i = 0; i < 10; i++)
         {
             Debug.Log(i);
@@ -38,22 +40,123 @@ public class Scope : MonoBehaviour
          */
 
         // Debug.Log(myInt);
+
         /*
          * if the line below is uncommented
          * then the line above works.
          */
 
-        Debug.Log(this.myInt);
         int myInt = 2; // stomps on class scoped myInt
         Debug.Log(myInt);
-
         /* However, declaring myInt in the Start() function
          * hides the class scoped version of myInt;
          */
-	}
 
-	void Update ()
+        Debug.Log(this.myInt);
+        /* Using the this keyword
+         * tells C# to look at the class
+         * scope to find the variable myInt
+         */
+
+
+        int declaredInStart = 1;
+        Debug.Log(declaredInStart);
+
+        /*
+         * Section 4.8.1 continued...
+         */
+
+        int firstInt = 100;
+        int secondInt = 200;
+        Debug.Log(firstInt);
+        Debug.Log(secondInt);
+
+        int thirdInt = 300;
+        Debug.Log(thirdInt);
+        int fourthInt = 400;
+        Debug.Log(fourthInt);
+    }
+
+
+    void Update ()
     {
+        //Debug.Log(declaredInStart);
+        /* uncomment the line above to see the error
+         */
+
         Debug.Log(myInt);
 	}
+
+    /*
+     * Section 4.8.2 Function Scope
+     */
+
+    void SomeFunction()
+    {
+         //public int fifthInt;
+         /* Uncomment the line above
+          * to see the error that the line
+          * produces.
+          */
+    }
+
+    /*
+     * Sectopm 4/8/2 continued...
+     * 
+     * Scope is how visible an object is
+     * so if a class as int i declared
+     * all functions in it also get to see
+     * i.
+     *     Start() doesn't see what
+     *     is inside of Update()
+     *    ╔══ class ════════════╗╔══ class ════════════╗
+     *    ║ ┌─ Start() ───────┐ ║║ ┌─ Start() ───────┐ ║
+     *    ║ │  int myInt;     │ ║║ │ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ │ ║
+     *    ║ └─────────────────┘ ║║ └─────────────────┘ ║
+     *    ║ ┌─ Update() ──────┐ ║║ ┌─ Update() ──────┐ ║
+     *    ║ │ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ │ ║║ │  int myInt;     │ ║
+     *    ║ └─────────────────┘ ║║ └─────────────────┘ ║
+     *    ╚═════════════════════╝╚═════════════════════╝
+     *    Likewise, Update() doesn't
+     *    see what's going on in Start()
+     *    
+     *    ╔══ class ════════════╗
+     *    ║          int i;     ║
+     *    ║ ┌─ function ─↓────┐ ║
+     *    ║ │            i    │ ║
+     *    ║ │  function gets  │ ║
+     *    ║ │  to see i.      │ ║
+     *    ║ └─────────────────┘ ║
+     *    ╚═════════════════════╝
+     *    variables in the class are visible
+     *    to the functions in the class.
+     *    ╔══ class ════════════╗
+     *    ║ ┌─ function ──────┐ ║
+     *    ║ │  int j;         │ ║
+     *    ║ └──────▬──────────┘ ║
+     *    ║ class doesn't get   ║
+     *    ║ to see j declared   ║
+     *    ║ in a function.      ║ 
+     *    ╚═════════════════════╝
+     *    
+     *    class Scope
+     *    {╔═══════════════════════════════╗
+     *     ║ int ClassInt;────────┐        ║
+     *     ║ void Start()         │        ║
+     *     ║ {╔═══════════════════│══════╗ ║
+     *     ║  ║ int myInt;        ↓      ║ ║
+     *     ║  ║ while(myInt < ClassInt)) ║ ║
+     *     ║  ║ {╔══════════════════╗    ║ ║
+     *     ║  ║  ║ Debug.Log(myInt);║    ║ ║
+     *     ║  ║  ║ myInt++;         ║    ║ ║
+     *     ║  ║ }╚══════════════════╝    ║ ║
+     *     ║ }╚══════════════════════════╝ ║
+     *     ║ void Update()                 ║
+     *     ║ {╔══════════════════════════╗ ║
+     *     ║  ║ int myInt; (can also see ║ ║
+     *     ║  ║                 ClassInt)║ ║
+     *     ║ }╚══════════════════════════╝ ║
+     *    }╚═══════════════════════════════╝
+     *    
+     */
 }
