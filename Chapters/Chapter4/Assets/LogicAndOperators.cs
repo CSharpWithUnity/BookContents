b610924f1704b7d8e3fa3e5a25748711e9464461
CSharpWithUnity <learningcsharp@okita.io>
2018-08-26 14:27:59 -0700

    adding more comments to section 4.11

--- Chapters/Chapter4/Assets/LogicAndOperators.cs
+++ Chapters/Chapter4/Assets/LogicAndOperators.cs
@@ -15,9 +15,49 @@ public class LogicAndOperators : MonoBehaviour
      */
 
     public bool someBool;
-
     /*
      * This value appears in the Inspector
      * panel.
      */
+
+    /*
+     * Some plain old data types
+     * are always initialized to
+     * zero.
+     */
+
+    //public int Gold;
+
+    /* Uncomment the line above to see
+     * what it's default initialization
+     * is.
+     * 
+     * The above gets initialized
+     * to 0 even though it's not
+     * assigned anything when declared.
+     */
+
+
+
+    /*
+     * Section 4.11.2 Equality Operators
+     */
+
+    void Start()
+    {
+        /*
+         * Section 4.11.2.1
+         */
+
+        someBool = (1 == 1);
+
+        int a = 1;
+        int b = 1;
+        someBool = (a == b);
+
+        int c = 1;
+        int d = 3;
+        someBool = (c == d);
+
+    }
 }
