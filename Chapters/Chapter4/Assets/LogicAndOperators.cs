65110d892504e9fe9276c8cd358cfc8256b1a9cf
CSharpWithUnity <learningcsharp@okita.io>
2018-08-26 02:13:39 -0700

    adding lots of Logic and Operations stuff, and some game content

---
+++ Chapters/Chapter4/Assets/LogicAndOperators.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Chapter 4.11 Logic and Operators
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+using UnityEngine;
+
+public class LogicAndOperators : MonoBehaviour
+{
+    /*
+     * Section 4.11.1 Booleans
+     */
+
+    public bool someBool;
+
+    /*
+     * This value appears in the Inspector
+     * panel.
+     */
+}
