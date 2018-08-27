2ba8fb8b7f50262ecc868962b13090701b4479bd
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-08-27 16:11:21 -0700

    added loops in loops section 4.12.8

--- Chapters/Chapter4/Assets/UsingLoops.cs
+++ Chapters/Chapter4/Assets/UsingLoops.cs
@@ -11,18 +11,76 @@ using UnityEngine;
 
 public class UsingLoops : MonoBehaviour
 {
-    int numCubes = 0;
+    public int numCubes = 10;
     private void Start()
     {
-        /*
-         * Section 4.12.6 Using Loops
-         */
-
-        while (numCubes < 10)
         {
-            GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            box.transform.position = new Vector3(numCubes * 2.0f, 0, 0);
-            numCubes++;
+            /*
+             * Section 4.12.6 Using Loops
+             */
+
+            while (numCubes < 10)
+            {
+                GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                box.transform.position = new Vector3(numCubes * 2.0f, 0, 0);
+                numCubes++;
+            }
+        }
+        {
+            /*
+             * Section 4.12.7 Loops within Loops
+             */
+            for (int i = 0; i < numCubes; i++)
+            {
+                for (int j = 0; j < numCubes; j++)
+                {
+                    GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    box.transform.position = new Vector3(i * 2.0f, j * 2.0f, 0);
+                }
+            }
+        }
+        {
+            /*
+             * Section 4.12.8 Runaway Loops
+             */
+
+            while (true)
+            {
+                //return;
+                /*
+                 * return here stops the entire function
+                 * and the code under here will not be
+                 * reached.
+                 */
+                break;
+            }
+
+            for (; ; )
+            {
+                break;
+            }
+
+            Debug.Log("Starting");
+            for (; ; )
+            {
+                Debug.Log("before break");
+                break;
+                //return;
+                Debug.Log("after the break");
+            }
+            Debug.Log("Ending");
+
+            int counter = 0;
+            while (true)
+            {
+                counter++;
+                if (counter > 10)
+                {
+                    Debug.Log("Break!");
+                    break;
+                }
+            }
+
         }
     }
 }
