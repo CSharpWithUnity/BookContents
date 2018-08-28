7b34b25e4f0d7a522aafed6bf6b15cd5566b17cd
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-08-27 23:40:36 -0700

    adding chapter5

---
+++ Chapters/Chapter5/Assets/Review.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * Chapter 5.2 Review
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+using UnityEngine;
+public class Review : MonoBehaviour
+{
+
+}
