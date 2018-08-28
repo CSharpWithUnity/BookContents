f4ed0920993f4c0726d01288abea46e82eef2e61
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-08-27 18:01:18 -0700

    adding code for section 4.13 scope again

---
+++ Chapters/Chapter4/Assets/OtherScope.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Chapter 4.13 Scope Again
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+using UnityEngine;
+public class OtherScope : MonoBehaviour
+{
+    /*
+     * Section 4.13 Scope Again
+     */
+    public float Size;
+    //Accessible to any other class
+
+    Vector3 mScale;
+
+    private void Update()
+    {
+        mScale = new Vector3(Size, Size, Size);
+        transform.localScale = mScale;
+    }
+}
+ 
--- Chapters/Chapter4/Assets/ScopeAgain.cs
+++ Chapters/Chapter4/Assets/ScopeAgain.cs
@@ -1,16 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿/*
+ * Chapter 4.13 Scope Again
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+ using UnityEngine;
 
-public class ScopeAgain : MonoBehaviour {
+public class ScopeAgain : MonoBehaviour
+{
+    /*
+     * Section 4.13.1 Visibility or Accessibility
+     * 
+     * accessibility keywords:
+     * private
+     * public
+     * protected
+     * internal
+     */
+    public bool SomeBool;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    /*
+     * Section 4.13.1.1 A Basic Example
+     */
+    OtherScope other;
+    private void Start()
+    {
+        SomeBool = true;
+        other = (OtherScope)GameObject.FindObjectOfType(typeof(OtherScope));
+        Debug.Log(other.gameObject.name);
+    }
+
+    public float otherScale;
+    private void Update()
+    {
+        other.Size = otherScale;
+    }
+
+    private bool hiddenBool;
+    bool alsoHidden;
+
+    private int privateInt;
+    public int publicInt;
+    protected int protectedInt;
+    internal int internalInt;
+
+
 }
