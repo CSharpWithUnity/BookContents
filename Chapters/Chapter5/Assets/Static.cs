29927b57f6daa834f5087472f59811d0cf541877
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-08-28 20:37:37 -0700

    adding zombie and static for chapter 5.5

--- Chapters/Chapter5/Assets/Static.cs
+++ Chapters/Chapter5/Assets/Static.cs
@@ -1,16 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/*
+ * Chapter 5.5 Static
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 using UnityEngine;
 
-public class Static : MonoBehaviour {
+public class Static : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update()
+    {
+        {
+            /*
+             * Section 5.5.1 A Basic Example
+             */
+
+            bool aKey = Input.GetKey(KeyCode.A);
+            if (aKey)
+            {
+                Debug.Log("aKey");
+            }
+        }
+        {
+            /*
+             * Section 5.5.1 continued...
+             */
+            Input input = new Input();
+            //bool aKey = input.GetKey(KeyCode.A);
+            /*
+             * Uncomment the line above to see the error
+             */
+        }
+        {
+            /*
+             * Section 5.5.2 Static Variables
+             */
+            for (int i = 0; i < 10; i++)
+            {
+                //Debug.Log(i); // prints 1 to 10
+                //uncomment the line above to see the numbers
+            }
+            // i no longer exists.
+            // Debug.Log(i); //i is undefined.
+        }
+
+    }
 }
---
+++ Chapters/Chapter5/Assets/Zombie.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Chapter 5.5.2.1 A Basic Example
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    public static int numZombies;
+    /*
+     * remove the static keyword above
+     * to see a different result
+     */
+	void Start ()
+    {
+        numZombies++;
+        Debug.Log(numZombies);
+	}
+
+    public static void CountZombies()
+    {
+        Debug.Log(numZombies);
+    }
+}
+
