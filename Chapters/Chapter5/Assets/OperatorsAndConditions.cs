d1cd0cae41c60cb01b5e0a8cbf6b5c70c65354df
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-08-29 23:46:02 -0700

    cleaning some project stuff

--- Chapters/Chapter5/Assets/OperatorsAndConditions.cs
+++ Chapters/Chapter5/Assets/OperatorsAndConditions.cs
@@ -1,16 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/*
+ * Chapter 5.8 Operators and Conditions
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 using UnityEngine;
 
-public class OperatorsAndConditions : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class OperatorsAndConditions : MonoBehaviour
+{
+    /*
+     * Section 5.8 Operators and Conditions
+     */
+    private void Start()
+    {
+        float temp = 90f;
+        bool sunny = true;
+        if (temp > 70f && sunny)
+        {
+            Debug.Log("Time to go swimming!");
+        }
+    }
 }
+
