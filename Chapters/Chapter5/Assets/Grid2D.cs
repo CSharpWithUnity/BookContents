ccbd56c0eeedcf80213bd556c0411f812cc27065
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-08-30 15:02:52 -0700

    adding grid 2d stuff for chapter 5.12

---
+++ Chapters/Chapter5/Assets/Grid2D.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Chapter 5.11 Multidimensional Arrays
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+ using UnityEngine;
+
+public class Grid2D : MonoBehaviour
+{
+    public int Width;
+    public int Height;
+    public GameObject PuzzlePiece;
+    public GameObject[,] Grid;
+}
+
