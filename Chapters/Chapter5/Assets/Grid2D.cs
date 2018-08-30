e8649a2624028f2cc61107b109fbbedd0da5917e
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-08-30 15:43:38 -0700

    updating some stuff for grid 2d

--- Chapters/Chapter5/Assets/Grid2D.cs
+++ Chapters/Chapter5/Assets/Grid2D.cs
@@ -7,13 +7,29 @@
  * of the MIT license.  See the LICENSE file for details.
  */
 
- using UnityEngine;
-
+using UnityEngine;
 public class Grid2D : MonoBehaviour
 {
     public int Width;
     public int Height;
     public GameObject PuzzlePiece;
     public GameObject[,] Grid;
+    /*
+     * Section 5.11.2 A Puzzle Board
+     */
+    void Start()
+    {
+        Grid = new GameObject[Width, Height];
+        for (int x = 0; x < Width; x++)
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                Vector3 position = new Vector3(x, y, 0);
+                GameObject go = GameObject.Instantiate(PuzzlePiece) as GameObject;
+                go.transform.position = position;
+                Grid[x, y] = go;
+            }
+        }
+    }
 }
 
