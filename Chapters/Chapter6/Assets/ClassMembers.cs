89e88ac573b5cb1aba3bde8da05b2979bf3344d4
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-09-01 00:53:12 -0700

    updates to 6.3.3

--- Chapters/Chapter6/Assets/ClassMembers.cs
+++ Chapters/Chapter6/Assets/ClassMembers.cs
@@ -19,6 +19,9 @@ public class ClassMembers : MonoBehaviour
      */
     public class Members
     {
+        /*
+         * Some public function in Members
+         */
         public void FirstFunction()
         {
             Debug.Log("FirstFunction");
@@ -27,8 +30,53 @@ public class ClassMembers : MonoBehaviour
 
     void Start()
     {
+        // Make a members object
         Members m = new Members();
+        // Call on Members' FirstFunction();
         m.FirstFunction();
+        position = transform.position;
+        /*
+         * Section 6.3.3.1 Return - A Basic Example
+         */
+        Debug.Log(ImANumber());
+        Debug.Log(ImANumber() + ImANumber());
     }
+    
+    int ImANumber()
+    {
+        return 3;
+    }
+
+    public Vector3 position;
+    void Update()
+    {
+        /*
+         * Section 6.3.2.2 Thinking like a programmer
+         */
+        bool AKey = Input.GetKey(KeyCode.A);
+        bool WKey = Input.GetKey(KeyCode.W);
+        bool SKey = Input.GetKey(KeyCode.S);
+        bool DKey = Input.GetKey(KeyCode.D);
+        Debug.Log(AKey);
+        if (AKey)
+        {
+            position.x = position.x - 0.1f;
+        }
+        if (DKey)
+        {
+            position.x = position.x + 0.1f;
+        }
+        if (WKey)
+        {
+            position.z = position.z + 0.1f;
+        }
+        if (SKey)
+        {
+            position.z = position.z - 0.1f;
+        }
+        transform.position = position;
+
+    }
+
 }
 
