5555bb5399868cdd64589ccd69e4783e00a111aa
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-09-03 16:15:27 -0700

    added new Monster, and updated chapter 6.5 examples

---
+++ Chapters/Chapter6/Assets/Monster.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Chapter 6.5 Arrays Revisited
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+using UnityEngine;
+public class Monster : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Im Alive!");
+    }
+}
