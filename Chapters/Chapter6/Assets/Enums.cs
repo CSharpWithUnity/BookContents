c81d54d445ea8ef82fa7a26b0b20eff7279cc4b6
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-09-03 20:16:27 -0700

    adding 6.6

--- Chapters/Chapter6/Assets/Enums.cs
+++ Chapters/Chapter6/Assets/Enums.cs
@@ -1,16 +1,94 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/*
+ * Chapter 6.6 Enums
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 using UnityEngine;
 
-public class Enums : MonoBehaviour {
+public class Enums : MonoBehaviour
+{
+    /*
+     * Section 6.6.1 Using Enums
+     */
+    public PrimitiveType MyPrimitiveType;
+    /*
+     * Right Click on PrimitiveType to go to the
+     * definition and see what the enum looks like.
+     */
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    enum YesOrNo
+    {
+        yes,
+        no
+    };
+    YesOrNo yesOrNoEnum;
+
+    /*
+     * Section 6.6.1 Continued...
+     */
+    public enum MyColors
+    {
+        red,
+        blue,
+        green
+    };
+    // also valid
+    // public enum MyColors {red, blue, green };
+    public MyColors MyColor;
+    GameObject gameObject;
+    private void Start()
+    {
+        {
+            /*
+             * Section 6.6.1 Using Enums
+             */
+            if (yesOrNoEnum == YesOrNo.yes)
+            {
+                Debug.Log("yes");
+            }
+            bool yesOrNoBool = true;
+            if (yesOrNoBool == true)
+            {
+                Debug.Log("yes");
+            }
+            int primitiveInt = 0;
+            /* 0 = Sphere,
+             * 1 = Capsule,
+             * 2 = Cylinder,
+             * 3 = Cube,
+             * 4 = Plane,
+             * 5 = Quad
+             */
+            if (primitiveInt == 0)
+            {
+                Debug.Log("Sphere");
+            }
+
+        }
+
+        {
+            /*
+             * Section 6.6.1 Continued...
+             */
+            gameObject = GameObject.CreatePrimitive(MyPrimitiveType);
+            MeshRenderer meshRenderer = gameObject.GetComponent(typeof(MeshRenderer)) as MeshRenderer;
+            Material material = meshRenderer.material;
+            if (MyColor == MyColors.red)
+            {
+                material.color = Color.red;
+            }
+            if (MyColor == MyColors.blue)
+            {
+                material.color = Color.blue;
+            }
+            if (MyColor == MyColors.green)
+            {
+                material.color = Color.green;
+            }
+        }
+    }
 }
--- Chapters/Chapter6/Assets/Monster.cs
+++ Chapters/Chapter6/Assets/Monster.cs
@@ -14,6 +14,15 @@ public class Monster : MonoBehaviour
     void Start()
     {
         Debug.Log("Im Alive!");
+        MonsterState = MonsterStates.standing;
+
+        {
+            /*
+             * Section 6.6.4 What We've Learned
+             */
+            int number = (int)MonsterState;
+            Debug.Log(number);
+        }
     }
 
     /*
@@ -23,5 +32,39 @@ public class Monster : MonoBehaviour
     {
         float wave = Mathf.Sin(Time.fixedTime + ID);
         transform.position = new Vector3(ID * Spacing, wave, 0);
+
+        {
+            /*
+             * Section 6.6.2 Combining What We've Learned
+             */
+            if (MonsterState == MonsterStates.standing)
+            {
+                Debug.Log("Standing monster is standing...");
+            }
+            if (MonsterState == MonsterStates.wandering)
+            {
+                Debug.Log("Wandering monster is wandering...");
+            }
+            if (MonsterState == MonsterStates.chasing)
+            {
+                Debug.Log("Chasing monster is chasing...");
+            }
+            if (MonsterState == MonsterStates.attacking)
+            {
+                Debug.Log("Attacking monster is attacking...");
+            }
+        }
     }
+
+    /*
+     * Section 6.6.2 Combining What We've Learned
+     */
+    public enum MonsterStates
+    {
+        standing,
+        wandering,
+        chasing,
+        attacking
+    }
+    public MonsterStates MonsterState;
 }
