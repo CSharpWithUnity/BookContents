6a017daf60cf1fa7d744f86047888b0926ac9ae1
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-09-09 01:51:26 -0700

    adding new stuff for using namespaces

---
+++ Chapters/Chapter6/Assets/ZombieMonster.cs
@@ -0,0 +1,16 @@
+﻿namespace ZombieGame
+{
+    public class ZombieMonster
+    {
+        public MonsterInfo zombieInfo;
+        public ZombieMonster()
+        {
+            zombieInfo = new MonsterInfo()
+            {
+                Health = 10,
+                Armor = 10,
+                AttackPower = 1
+            };
+        }
+    }
+}
