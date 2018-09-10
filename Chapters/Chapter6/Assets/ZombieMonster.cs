7d061b03ca291fd32ec089bcadccd208141b013e
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-09-09 21:12:04 -0700

    updating last bits of chapter 6.10

--- Chapters/Chapter6/Assets/ZombieMonster.cs
+++ Chapters/Chapter6/Assets/ZombieMonster.cs
@@ -1,4 +1,13 @@
-﻿namespace ZombieGame
+﻿/*
+ * Chapter 6.10.5 Putting Namespaces to Work
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+namespace ZombieGame
 {
     public class ZombieMonster
     {
