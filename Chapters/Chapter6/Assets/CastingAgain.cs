fb31b2c9c8f52bc734c4f825d63fe64f64a2b719
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-09-16 13:45:59 -0700

    adding content for section 6.14

---
+++ Chapters/Chapter6/Assets/CastingAgain.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Chapter 6.14 Type Casting Again
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+using UnityEngine;
+
+public class CastingAgain : MonoBehaviour
+{
+    /*
+     * Section 6.14 Type Casting Again
+     */
+    enum SimpleEnums
+    {
+        FirstValue,
+        SecondValue,
+        ThirdValue
+    }
+
+    void Start()
+    {
+        SimpleEnums simpleEnum = SimpleEnums.SecondValue;
+        int convertedEnum = simpleEnum as int;
+    }
+}
+
