34fdf1ed20f1aa95d30d2611b02fd58a47ff4986
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-09-27 11:22:26 -0700

    mornings up date

--- Chapters/Chapter6/Assets/MoreOnArrays.cs
+++ Chapters/Chapter6/Assets/MoreOnArrays.cs
@@ -15,24 +15,43 @@ public class MoreOnArrays : MonoBehaviour
     {
         {
             /*
+             * Section 6.17.1 More On Arrays
+             */
+
+            int i;
+            /* a single int value */
+            int[] ints;
+            /* array of ints without any values */
+            //Debug.Log(i + ints[0]);
+            /* Cannot use i or ints without values */
+
+            int[] primes = new int[] { 1, 3, 5, 7, 11, 13, 17, 23, 27, 31 };
+            /*  ↑                  ↑              */
+            /* ┌┴──────────────────┴────────┐     */
+            /* │ brackets tell lexer to     │     */
+            /* │ create array for given type│     */
+            /* └────────────────────────────┘     */
+        }
+        {
+            /*
              * Section 6.17.1.1 A Basic Example More On Arrays continued...
              */
             int[] primes = new int[] { 1, 3, 5, 7, 11, 13, 17, 23, 27, 31 };
-            int arrayLength = primes.Length;
-            /*                          ↑               */
-            /*               ┌──────────┴────────────┐  */
-            /*               │ returns the number of │  */
-            /*               │ items in the array.   │  */
-            /*               └──────────┬────────────┘  */
-            /*                          ↓               */
-            for (int i = 0; i < arrayLength; i++)
+            int primesLength = primes.Length;
+            /*      ↓   └── ← ───┘      ↑               */
+            /*   ┌──┴───┐    ┌──────────┴────────────┐  */
+            /*   │ used │    │ returns the number of │  */
+            /*   └──┬───┘    │ items in the array.   │  */
+            /*      ↓        └───────────────────────┘  */
+            /*      └────── → ──────┐                   */
+            for (int i = 0; i < primesLength; i++)
             {                        /* ↓               */
-                Debug.Log(primes[i]);/* │ reusing       */
-            }                        /* │ the array     */
-                                     /* │ length        */
-                                     /* │ value         */
+                Debug.Log(primes[i]);/* │ using         */
+            }                        /* │ arrayLength   */
+                                     /* │ value again   */
+                                     /* │               */
             int j = 0;               /* │               */
-            while (j < arrayLength)  /*←┘               */
+            while (j < primesLength)/*←─┘               */
             {
                 Debug.Log(primes[j++]);
             }
@@ -64,11 +83,12 @@ public class MoreOnArrays : MonoBehaviour
             }
         }
         {
-
+            /* with some value types you can just assign them without new type[]*/
+            /*  omitted   ↓ new int[], instead we're just assigning values      */
             int[] primes = { 1, 3, 5, 7, 11, 13, 17, 23, 27, 31 };
             int[] fibonacci = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 };
             int[] powersOfTwo = { 1, 2, 4, 8, 16, 32, 64, 128, 255, 512, 1024 };
-
+            object[] numbers = { primes, fibonacci, powersOfTwo};
         }
     }
 }
