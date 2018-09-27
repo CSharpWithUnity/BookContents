3d1b306f4f816010f67f5ecb044de7dee7f12900
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-09-26 19:54:26 -0700

    added new sections for section 6.17

--- Chapters/Chapter6/Assets/MoreOnArrays.cs
+++ Chapters/Chapter6/Assets/MoreOnArrays.cs
@@ -18,24 +18,45 @@ public class MoreOnArrays : MonoBehaviour
              * Section 6.17.1.1 A Basic Example More On Arrays continued...
              */
             int[] primes = new int[] { 1, 3, 5, 7, 11, 13, 17, 23, 27, 31 };
-            int primesLength = primes.Length;
-
-            for (int i = 0; i < primesLength; i++)
-            {
-                Debug.Log(primes[i]);
-            }
-
-            int j = 0;
-            while (j < primesLength)
+            int arrayLength = primes.Length;
+            /*                          ↑               */
+            /*               ┌──────────┴────────────┐  */
+            /*               │ returns the number of │  */
+            /*               │ items in the array.   │  */
+            /*               └──────────┬────────────┘  */
+            /*                          ↓               */
+            for (int i = 0; i < arrayLength; i++)
+            {                        /* ↓               */
+                Debug.Log(primes[i]);/* │ reusing       */
+            }                        /* │ the array     */
+                                     /* │ length        */
+                                     /* │ value         */
+            int j = 0;               /* │               */
+            while (j < arrayLength)  /*←┘               */
             {
                 Debug.Log(primes[j++]);
             }
         }
         {
             /*
+             * Section 6.17.1.1 continued.
+             */
+            int[] primes = new int[] { 1, 3, 5, 7, 11, 13, 17, 23, 27, 31 };
+
+            /*               ┌───────────────────────┐  */
+            /*               │ Length needs to be    │  */
+            /*               │ requested every loop  │  */
+            /*               └──────────┬────────────┘  */
+            /*                          ↓               */
+            for (int i = 0; i < primes.Length; i++)
+            {
+                Debug.Log(primes[i]);
+            }
+        }
+        {
+            /*
              * Section 6.17.2 Foreach a reminder
              */
-
             int[] primes = new int[] { 1, 3, 5, 7, 11, 13, 17, 23, 27, 31 };
             foreach (int p in primes)
             {
@@ -43,6 +64,7 @@ public class MoreOnArrays : MonoBehaviour
             }
         }
         {
+
             int[] primes = { 1, 3, 5, 7, 11, 13, 17, 23, 27, 31 };
             int[] fibonacci = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 };
             int[] powersOfTwo = { 1, 2, 4, 8, 16, 32, 64, 128, 255, 512, 1024 };
