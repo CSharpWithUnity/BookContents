fb5cda8d9085d3b13bd45eadef0e71ade6a0e8af
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-09-29 22:10:25 -0700

    cleaning up some packages, and adding some new examples for out

--- Chapters/Chapter6/Assets/OutParameter.cs
+++ Chapters/Chapter6/Assets/OutParameter.cs
@@ -7,7 +7,7 @@
  * of the MIT license.  See the LICENSE file for details.
  */
 using System.Collections;
- using UnityEngine;
+using UnityEngine;
 
 public class OutParameter : MonoBehaviour
 {
@@ -54,22 +54,72 @@ public class OutParameter : MonoBehaviour
      /* ❶ seven comes in, gets assigned 7 */
      /* ❷ then is sent back out           */
 
+    void GetThreeValuesOut(out int first, out int second, out int third)
+    {/*   ┌←❶──────────────────────←┘ ↑            │ ↑             │ ↑  */
+        first = 1;/* assigned 1       │            │ │             │ │  */
+     /*   └→❷──────────────────────→──┘            │ │             │ │  */
+     /*   ┌←❶──────────────────────←───────────────┘ │             │ │  */
+        second = 2;/* assigned 2                     │             │ │  */
+     /*   └→❷──────────────────────→─────────────────┘             │ │  */
+     /*   ┌←❶──────────────────────←───────────────────────────────┘ │  */
+        third = 3;/*  assigned 3                                     │  */
+     /*   └→❷──────────────────────→─────────────────────────────────┘  */
+    }
+
+
+    void GoingInAndOut(int inValue, out int outValue)
+    {/*              ┌←❶────←┘                 ↑    */
+        outValue = inValue * 2;/*              │    */
+     /*    └→❷────────────────────────────────→┘    */
+    }/*  outValue is set to inValue multiplied by 2 */
+
     void Start()
     {
         {
             /*
              * Section 6.18 OutParameter
              */
-    GettingSevenEleven();
+            GettingSevenEleven();
             /* Showing various simple ways to get values from functions */
         }
         {
             /*
              * Section 6.18.1 Out Parameter, A Basic Example continued...
              */
-            int i;
+
+            int i = 0;/* int i goes in as 0    */
+            /*  └❶→─────────┐  i = 0           */
             GetSevenOut(out i);
-            Debug.Log(i);
+            /*        ┌←❷───┘  i = 7           */
+            Debug.Log(i);/* comes out as 7     */
+        }
+        {
+            /* this works with multiple values               */
+            int i, j, k;
+            /*  │  │  └❶→───────────────────────┐            */
+            /*  │  └❶→───────────────────┐      │            */
+            /*  └❶→───────────────┐i = 0 │j = 0 │k = 0       */
+            GetThreeValuesOut(out i, out j, out k);
+            /*                ┌───┘i = 1 │j = 2 │k = 3       */
+            /*                │          └─┐    └───────┐    */
+            /*                ❷            ❷            ❷    */
+            /*                ↓            ↓            ↓    */
+            Debug.Log("i: " + i + " j: " + j + " k: " + k);
+        }
+        {
+            int i = 0;
+            /*  └❶→──────────────┐  i = 0           */
+            GoingInAndOut(6, out i);
+            /*        ┌←❷────────┘  i = 12          */
+            Debug.Log(i);/* comes out as 12         */
+        }
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit raycastHit;
+
+            Physics.Raycast( ray, out raycastHit);
+            
+            Debug.Log(raycastHit);
         }
     }
 }
