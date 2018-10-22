abbe7a98c136c904eb09a1121fba83f9efa8caf2
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-10-21 21:13:05 -0700

    adding 7.10 content

---
+++ Chapters/Chapter7/Assets/ClassConstructors.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Chapter 7.10 Class Constructors Revisited
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+using UnityEngine;
+
+public class ClassConstructors : MonoBehaviour
+{
+    public enum ObstacleType
+    {
+        TreadMill_Empty,
+        TreadMill_Center,
+        TreadMill_Left,
+        TreadMill_Right
+    }
+
+    class TreadMillSegment
+    {
+        static int Segments;
+        private int Step;
+        private float Speed;// Distance Traveled per step
+        private GameObject Segment;
+        public TreadMillSegment(float speed, ObstacleType obstacle, ref TreadmillUpdate updater)
+        {
+            Speed = speed;
+            updater += Updated;
+            Segment = Instantiate(Resources.Load(obstacle.ToString()) as GameObject);
+            Step = Segments * (int)(1 / Speed);
+            Segments++;
+        }
+
+        void Updated()
+        {
+            Segment.transform.position = new Vector3()
+            {
+                z = -(float)(Step++%(Segments * ((int)(1 / Speed)))) * Speed
+            };
+        }
+    }
+
+    private delegate void TreadmillUpdate();
+    private TreadmillUpdate DoUpdate;
+    void Start()
+    {
+        int numSegments = 7;
+        for (int i = 0; i < numSegments; i++)
+        {
+            ObstacleType obstacle = (ObstacleType)(i%4);
+            new TreadMillSegment(0.05f, obstacle, ref DoUpdate);
+        }
+    }
+    
+    void Update()
+    {
+        DoUpdate?.Invoke();
+    }
+}
