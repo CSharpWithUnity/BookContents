ee551d648fa0f90ff166d6823ec300925fa34ee9
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-10-22 01:51:34 -0700

    adding new treadmill controller

--- Chapters/Chapter7/Assets/ClassConstructors.cs
+++ Chapters/Chapter7/Assets/ClassConstructors.cs
@@ -6,19 +6,24 @@
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
  */
+using System;
 using UnityEngine;
 
 public class ClassConstructors : MonoBehaviour
 {
+    /*
+     * Section 7.10 Class Constructors
+     */
+    
     enum ObstacleType
     {
-        TreadMill_Empty,
-        TreadMill_Center,
-        TreadMill_Left,
-        TreadMill_Right
+        Treadmill_Empty,
+        Treadmill_Center,
+        Treadmill_Left,
+        Treadmill_Right
     }
 
-    class TreadMillSegment
+    class TreadmillSegment
     {
         static int Segments;
         private float Speed;
@@ -26,7 +31,7 @@ public class ClassConstructors : MonoBehaviour
         private int Step;
         private GameObject Segment;
 
-        public TreadMillSegment(float speed, ObstacleType obstacle, ref TreadmillUpdate treadmillUpdater)
+        public TreadmillSegment(float speed, ObstacleType obstacle, ref TreadmillUpdate treadmillUpdater)
         {
             Speed = speed;
             Segment = Instantiate(Resources.Load(obstacle.ToString()) as GameObject);
@@ -49,22 +54,55 @@ public class ClassConstructors : MonoBehaviour
 
     private delegate void TreadmillUpdate();
     private TreadmillUpdate UpdateTreadmill;
-
-    void Start()
+    void UseTreadmillSegments()
     {
         int numSegments = 7;
         for (int i = 0; i < numSegments; i++)
         {
-            ObstacleType obstacle = (ObstacleType)(i%4);
-            new TreadMillSegment(0.015f, obstacle, ref UpdateTreadmill);
+            ObstacleType obstacle = (ObstacleType)(i % 4);
+            new TreadmillSegment(0.015f, obstacle, ref UpdateTreadmill);
         }
     }
-    
-    void Update()
+
+    void UpdateTreadmillSegments()
     {
         if (UpdateTreadmill != null)
         {
             UpdateTreadmill();
         }
     }
+
+    /*
+     * Section 7.10.2 Classes that manage classes
+     */
+    class TreadmillManager
+    {
+        public void StartTreadmill()
+        {
+            
+        }
+
+        public void DestroyTreadmill()
+        {
+            
+        }
+
+        public void UpdateTreadmill()
+        {
+
+        }
+        public int Segments;
+        public float Speed;
+    }
+
+    void Start()
+    {
+        UseTreadmillSegments();
+    }
+
+    void Update()
+    {
+        UpdateTreadmillSegments();
+    }
+
 }
