54e7e0c9f400455aafea78f801d1f460414b38b6
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-10-24 23:39:05 -0700

    adding update to treadmill manager

--- Chapters/Chapter7/Assets/ClassConstructors.cs
+++ Chapters/Chapter7/Assets/ClassConstructors.cs
@@ -7,6 +7,7 @@
  * of the MIT license.  See the LICENSE file for details.
  */
 using System;
+using System.IO;
 using UnityEngine;
 
 public class ClassConstructors : MonoBehaviour
@@ -42,7 +43,7 @@ public class ClassConstructors : MonoBehaviour
             Segments++;
         }
 
-        void TreadmillUpdated()
+        public void TreadmillUpdated()
         {
             int totalSteps = Segments * StepSize;
             Segment.transform.position = new Vector3()
@@ -77,27 +78,115 @@ public class ClassConstructors : MonoBehaviour
      */
     class TreadmillManager
     {
-        public void StartTreadmill()
+        public readonly static string ProjectPath = System.IO.Directory.GetCurrentDirectory();
+        public static void WritePatternExample()
         {
+            string filePath = ProjectPath + "\\pattern.txt";
+            // filePath = C:\Users\[user name]\Documents\BookContents\Chapters\Chapter7\pattern.txt
+
+            StreamWriter writer = new StreamWriter(filePath);
+            string defaultPattern =
+                "Treadmill_Left\n" +
+                "Treadmill_Center\n" +
+                "Treadmill_Right\n" +
+                "Treadmill_Right\n" +
+                "Treadmill_Center\n" +
+                "Treadmill_Left";
+            // easier to read than a really long string.
+
+            writer.Write(defaultPattern);
+            writer.Close();
+        }
+
+        public static string ReadPattern()
+        {
+            string filePath = ProjectPath + "\\pattern.txt";
+            // filePath = C:\Users\[user name]\Documents\BookContents\Chapters\Chapter7\pattern.txt
+
+            StreamReader reader = new StreamReader(filePath);
+            string readPattern = reader.ReadToEnd();
+            /* reads something like "Treadmill_Left\nTreadmill_Center\nTreadmill_Right\n */
+            /* until it reaches the end of the file.                                     */
+            reader.Close();
+            return readPattern;
+        }
+        
+        public static string[] PatternToArray(string pattern)
+        {
+            //remove trailing '\n'
+            pattern.TrimEnd(new char[] { '\n' });
+            /* if there was a "Treadmill_Center\n" at the end this will */
+            /* cut it off.                                              */
+
+            string[] patternArray = pattern.Split('\n');
+            /* breaks this into string[]{ "Treadmill_Center", "Treadmill_Left"} */
+            /* an array with each string part between the \n chars.             */
+
+            return patternArray;
+        }
+
+        public static void AppendPattern(ObstacleType obstacle)
+        {
+            string previousPattern = ReadPattern();
+            /* previous string might be like "Treadmill_Center" since we    */
+            /* trim the trailing \n                                         */
             
-        }
+            string newPattern = previousPattern + "\n" + obstacle.ToString();
+            /* this adds a new "Treadmill_Left + "\nTreadmill_Center"       */
+            /* to the end of the previous string                            */
 
-        public void DestroyTreadmill()
+            string filePath = ProjectPath + "\\pattern.txt";
+            // filePath = C:\Users\[user name]\Documents\BookContents\Chapters\Chapter7\pattern.txt
+            StreamWriter writer = new StreamWriter(filePath);
+            writer.Write(newPattern);
+            writer.Close();
+        }
+    }
+
+    void UseTreadmillManger()
+    {
+        TreadmillManager.WritePatternExample();
+
+        TreadmillManager.AppendPattern(ObstacleType.Treadmill_Center);
+        TreadmillManager.AppendPattern(ObstacleType.Treadmill_Center);
+        TreadmillManager.AppendPattern(ObstacleType.Treadmill_Center);
+        TreadmillManager.AppendPattern(ObstacleType.Treadmill_Empty);
+        TreadmillManager.AppendPattern(ObstacleType.Treadmill_Empty);
+        TreadmillManager.AppendPattern(ObstacleType.Treadmill_Empty);
+
+        string pattern = TreadmillManager.ReadPattern();
+        // gets the pattern of segments from disk
+
+        string[] obstacleArray = TreadmillManager.PatternToArray(pattern);
+        // gets the converted string to string[]{ "Treadmill_Left", "Treadmill_Center" } etc...
+
+        int numSegments = obstacleArray.Length;
+        // get the number of segments in the array
+
+        for (int i = 0; i < numSegments; i++)
         {
-            
+            switch (obstacleArray[i])
+            {
+                case "Treadmill_Empty":
+                    new TreadmillSegment(0.015f, ObstacleType.Treadmill_Empty, ref UpdateTreadmill);
+                    break;
+                case "Treadmill_Center":
+                    new TreadmillSegment(0.015f, ObstacleType.Treadmill_Center, ref UpdateTreadmill);
+                    break;
+                case "Treadmill_Left":
+                    new TreadmillSegment(0.015f, ObstacleType.Treadmill_Left, ref UpdateTreadmill);
+                    break;
+                case "Treadmill_Right":
+                    new TreadmillSegment(0.015f, ObstacleType.Treadmill_Right, ref UpdateTreadmill);
+                    break;
+            }
         }
-
-        public void UpdateTreadmill()
-        {
-
-        }
-        public int Segments;
-        public float Speed;
     }
 
     void Start()
     {
-        UseTreadmillSegments();
+        //UseTreadmillSegments();
+        UseTreadmillManger();
     }
 
     void Update()
