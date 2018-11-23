cdb457cf5e01d9f1ac95b939673e17f5446356d0
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-11-22 18:30:42 -0800

    finishing linq stuff

--- Chapters/Chapter8/Assets/Linq.cs
+++ Chapters/Chapter8/Assets/Linq.cs
@@ -23,22 +23,53 @@ public class Linq : MonoBehaviour
      */
     void UseLinqOnArray()
     {
-        int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        var evenNums = from n in numbers
-                       where (n % 2) == 0
-                       select n;
-        // Linq adds the keywords, from
-        // where and select.
-        foreach (int n in evenNums)
         {
-            Debug.Log(n);
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var evenNums = from n in numbers
+                           where (n % 2) == 0
+                           select n;
+            // Linq adds the keywords, from
+            // where and select.
+            foreach (int n in evenNums)
+            {
+                Debug.Log(n);
+            }
+            // 0
+            // 2
+            // 4
+            // 6
+            // 8
+            // 10
+
         }
-        // 0
-        // 2
-        // 4
-        // 6
-        // 8
-        // 10
+        {
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            /*      └──────────────❶─────┐ numbers is an array    */
+            /*                  ┌──❷──┐  ↓ n is each object in    */
+            /*                  ↓     ↑  ↓ the number array.      */
+            var evenNums = from n in numbers
+            /*    ↑ the result  ↓                                 */
+            /*    ❺ is added to └❸┐ an operation is performed     */
+            /*    │ evenNums      ↓ on each object in the array   */
+            /*    │      */where (n % 2) == 0                     
+            /*    │               ↓ if this operation is true     */
+            /*    │               ❹ the value is added to         */
+            /*    │               ↓ the result of the statement   */
+            /*    └─────←*/select n;
+            // Linq adds the keywords, from
+            // where and select.
+            foreach (int n in evenNums)
+            {
+                Debug.Log(n);
+            }
+            // 0
+            // 2
+            // 4
+            // 6
+            // 8
+            // 10
+
+        }
     }
 
     /*
@@ -110,8 +141,8 @@ public class Linq : MonoBehaviour
         //declare an array
         int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         var divisibleByThree = from n in numbers 
-                   /*  └───┐ */where (n % 3) == 0
-                   /*      │ */select n;
+                   /*  ↓     */where (n % 3) == 0
+                   /*  └───┐ */select n;
         numbers[0] = 1;/*  │ */
         numbers[3] = 1;/*  │  changes made to numbers not to         */
         numbers[6] = 30;/* │  divisibleByThree but divisibleByThree  */
@@ -162,7 +193,6 @@ public class Linq : MonoBehaviour
             
             // Lambda
             Func<int, bool> even = (x) => { return x % 2 == 0; };
-            
             int[] setB = setA.Where(even).ToArray();
             foreach (int i in setB)
             {
@@ -226,6 +256,10 @@ public class Linq : MonoBehaviour
             // 6
         }
     }
+
+    /*
+     * Section 8.8.6 Monsters Linq
+     */
     
     public enum Alignments
     {
@@ -251,10 +285,16 @@ public class Linq : MonoBehaviour
             }
             Alignment = (Alignments)UnityEngine.Random.Range(0, 3);
             HP = UnityEngine.Random.Range(1, 11);
+            Position = new Vector3()
+            {
+                x = UnityEngine.Random.Range(-1f, 1f),
+                y = UnityEngine.Random.Range(-1f, 1f),
+                z = UnityEngine.Random.Range(-1f, 1f)
+            };
         }
     }
 
-    class MonsterNameComparer : IEqualityComparer<Monster>
+    class MonsterNameEqualityComparer : IEqualityComparer<Monster>
     {
         public bool Equals(Monster x, Monster y)
         {
@@ -267,107 +307,226 @@ public class Linq : MonoBehaviour
         }
     }
 
-    class MonsterAlignmentComparer : IEqualityComparer<Monster>
+    class MonsterNameComparer : IComparer<Monster>
     {
-        public bool Equals(Monster x, Monster y)
+        public int Compare(Monster x, Monster y)
         {
-            return x.Alignment == y.Alignment;
-        }
-
-        public int GetHashCode(Monster obj)
-        {
-            return obj.Alignment.GetHashCode();
-        }
-    }
-    class MonsterNameAlignmentComparer : IEqualityComparer<Monster>
-    {
-        public bool Equals(Monster x, Monster y)
-        {
-            return x.Name.Equals(y.Name) && x.Alignment == y.Alignment;
-        }
-
-        public int GetHashCode(Monster obj)
-        {
-            return obj.Name.GetHashCode() + obj.Alignment.GetHashCode();
+            return string.Compare(x.Name, y.Name);
         }
     }
 
-    void UseComparer()
+    class MonsterAlignmentComparer : IComparer<Monster>
+    {
+        public int Compare(Monster x, Monster y)
+        {
+            int a = x.Alignment.GetHashCode();
+            int b = y.Alignment.GetHashCode();
+            int i = 0;
+            if (a > b)
+                i = 1;
+            else if (a < b)
+                i = -1;
+            return i;
+            // or...
+            // return a > b ? 1 : a < b ? -1 : 0;
+        }
+    }
+
+    void UseComparerClasses()
     {
         int numMonsters = 100;
         Monster[] monsters = new Monster[numMonsters];
         for (int i = 0; i < numMonsters; i++)
         {
-            monsters[i] = new Monster(); 
+            monsters[i] = new Monster();
         }
+        {
+            var distinct = monsters.Distinct();
+            string log = "Distinct default: \n";
+            int count = 1;
+            foreach (Monster m in distinct)
+            {
+                log += count++ + ") name: " + m.Name + " " + m.Alignment + "\n";
+            }
+            // odds are that all 100 are still in the list.
+            Debug.Log(log);
+        }
+        {
+            /* this will use the Name Comparer to filter out
+             * monsters with the same name
+             */
+            MonsterNameEqualityComparer nameEqualer = new MonsterNameEqualityComparer();
+            var unique = monsters.Distinct(nameEqualer).ToArray();
+            //about a third of the names will be removed.
+            string log = "";
+            int count = 1;
+            foreach (Monster m in unique)
+            {
+                log += count++ + ") name: " + m.Name + " " + m.Alignment + "\n";
+            }
+            Debug.Log(log);
+        }
+        {
+            /* this will use the Name Comparer to sort the names
+             * then we filter out repeated names but ignores
+             * alignments.
+             */
+            MonsterNameComparer compareNames = new MonsterNameComparer();
+            MonsterNameEqualityComparer nameEqualer = new MonsterNameEqualityComparer();
+            var distinctOrdered = monsters.OrderBy(m => m, compareNames).Distinct(nameEqualer);
+
+            string log = "using classes to compare and filter: \n";
+            int count = 1;
+            foreach (Monster m in distinctOrdered)
+            {
+                log += count++ + ") name: " + m.Name + " " + m.Alignment + "\n";
+            }
+            Debug.Log(log);
+        }
+        {
+            /* this uses lambdas to sort the names
+             * then we filter out repeated names but ignores
+             * alignments.
+             */
+            var distinctOrdered = monsters.OrderBy(m => m.Name)
+                                          .GroupBy(m => m.Name)
+                                          .Select(group => group.First());
+
+            string log = "using lambda to compare and filter: \n";
+            int count = 1;
+            foreach (Monster m in distinctOrdered)
+            {
+                log += count++ + ") name: " + m.Name + " " + m.Alignment + "\n";
+            }
+            Debug.Log(log);
+        }
+        {
+            /* this does the same name sorting and
+             * also filters out repeats, but ignores
+             * alignment.
+             */
+            var distinctOrdered =  from monster in monsters
+                                   orderby monster.Name ascending
+                                   group monster by monster.Name into uniqueNames
+                                   select uniqueNames.First();
+
+            string log = "using linq to compare and filter \n";
+            int count = 1;
+            foreach (Monster m in distinctOrdered)
+            {
+                log += count++ + ") name: " + m.Name + " " + m.Alignment + "\n";
+            }
+            Debug.Log(log);
+        }
+        {
+            /* This produces the grouped data which is a Dictionary
+             * where the orderby turns the Name property into a Key
+             * to sort the array.
+             */
+            var distinctOrdered = from monster in monsters
+                                  orderby monster.Name ascending
+                                  group monster by monster.Name into uniqueNames
+                                  select uniqueNames;
+
+            string log = "What is in uniqueNames \n";
+            int count = 1;
+            foreach (var group in distinctOrdered)
+            {
+                var groupKey = group.Key;
+                foreach (Monster m in group)
+                {
+                    log += count++ + ")" + groupKey +" " + m.Alignment + "\n";
+                }
+            }
+            Debug.Log(log);
+        }
+    }
+
+    void UseLambdaComparer()
+    {
+        int numMonsters = 100;
+        Monster[] monsters = new Monster[numMonsters];
+        for (int i = 0; i < numMonsters; i++)
+        {
+            monsters[i] = new Monster();
+        }
+
         {
             /* This produces a list of all 100 monsters
              * they are grouped together based on their
-             * names and alignments. */
+             * names and alignments.
+             */
+            var sorted = monsters.OrderBy
+            (   
+                m =>
+                m.Alignment.GetHashCode() + m.Name.GetHashCode()
+            );
 
-            Monster[] sorted = monsters.OrderBy(m => m.Alignment.GetHashCode() + m.Name.GetHashCode()).ToArray();
-            string log = "";
-            int count = 1;
+            string log = "Sorted Monsters using lambda \n";
+            int n = 1;
             foreach (Monster m in sorted)
             {
-                log += count++ + ") name: " + m.Name + " " + m.Alignment + "\n";
+                log += n++ + ") name: " + m.Name + " " + m.Alignment + "\n";
             }
             Debug.Log(log);
-            {
-                Monster[] strongMonsters = monsters.Where(x => x.HP > 9 ).ToArray();
-                string l = "strongMonsters: \n";
-                count = 1;
-                foreach (Monster m in strongMonsters)
-                {
-                    l += count++ + ") " + m.Name + " " + m.HP + "\n";
-                }
-                Debug.Log(l);
-            }
-            {
-                var strongMonsters = from monster in monsters
-                           where monster.HP > 9
-                           select monster;
+        }
 
-                string l = "strongMonsters: \n";
-                count = 1;
-                foreach (Monster m in strongMonsters)
-                {
-                    l += count++ + ") " + m.Name + " " + m.HP + "\n";
-                }
-                Debug.Log(l);
+        {
+            /* This picks out monsters with HP above 9
+             * then lists them out.
+             */
+            var strongMonsters = monsters.Where
+            (
+                monster =>
+                monster.HP > 9
+            );
+            string log = "Strong Monsters: \n";
+            int n = 1;
+            foreach (Monster m in strongMonsters)
+            {
+                log += n++ + ") " + m.Name + " " + m.HP + "\n";
             }
+            Debug.Log(log);
         }
 
 
         {
-            /* this will use the Name Comparer to filter out    */
-            /* monsters with the same name                      */
-            Monster[] unique = monsters.Distinct(new MonsterNameComparer()).ToArray();
+            /* This picks out monsters with HP above 9
+             * then lists them out. This and the above
+             * statements are equivalent. The only difference
+             * is the syntax and use of keywords in place
+             * of anonymous lambda like syntax.
+             */
+            var strongMonsters = from monster in monsters
+                                 where monster.HP > 9
+                                 select monster;
 
-            //about a third of the names will be removed.
-            string log = "";
-            int count = 1;
-            foreach (Monster m in unique)
+            string log = "Strong Monsters: \n";
+            int n = 1;
+            foreach (Monster m in strongMonsters)
             {
-                log += count++ + ") name: " + m.Name + " " + m.Alignment + "\n";
+                log += n++ + ") " + m.Name + " " + m.HP + "\n";
             }
             Debug.Log(log);
         }
 
-        {   /* this will use the Name Comparer to filter out    */
-            /* monsters with the same name                      */
-            Monster[] unique = monsters.Distinct(new MonsterNameComparer()).OrderBy(x => x.Name.GetHashCode() ^ x.HP).ToArray();
+        {
+            /* Objects in the linq statement can see
+             * variables in its immediate scope
+             */
+            Monster ComparedTo = new Monster();/* →─────┐       ComparedTo can  */
+            var weakerThan = from monster in monsters/* ↓       be used in the  */
+                                where monster.HP < ComparedTo.HP/* where statement */
+                                select monster;
 
-            //about a third of the names will be removed.
-            string log = "";
-            int count = 1;
-            foreach (Monster m in unique)
+            string log = "Weaker than " + ComparedTo.Name + ":" + ComparedTo.HP + "\n";
+            int n = 1;
+            foreach (Monster m in weakerThan)
             {
-                log += count++ + ") name: " + m.Name + " " + m.Alignment + "\n";
+                log += n++ + ") " + m.Name + " " + m.HP + "\n";
             }
             Debug.Log(log);
         }
-
     }
 
     void Start()
@@ -411,6 +570,6 @@ public class Linq : MonoBehaviour
          * the function in action.
          */
         //UseLinqOnLists();
-        UseComparer();
+        UseComparerClasses();
     }
 }
