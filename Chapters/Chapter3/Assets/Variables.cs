7bfe91d8c194c391f40a985c5e40f30057fbaa55
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-12-01 12:59:13 -0800

    adding changes to match chapter 3

--- Chapters/Chapter3/Assets/Variables.cs
+++ Chapters/Chapter3/Assets/Variables.cs
@@ -38,63 +38,143 @@ class Identifiers
 
 }
 
-[UnityEngine.ExecuteInEditMode]
-class Variables : UnityEngine.MonoBehaviour
+namespace Variables
 {
-    /*
-     * Section 3.9.2 Data
-     * 
-     * A Declaration is the announcement to
-     * Unity about your new variable.
-     *
-     * ┌──────┐┌────────────┐┌───────────┐
-     * │ Type ││ Identifier ││ Separator │
-     * └─┬────┘└────┬───────┘└────┬──────┘
-     *   └─────┐    └┐            │
-     *         │     │    ┌───────┘
-     *       ──┴── ──┴─── ┴ 
-     *       float myFloat;
-     *       └────┬───────┘
-     *        ┌───┘
-     *  ┌─────┴─────────────────┐
-     *  │ Declaration Statement │
-     *  └───────────────────────┘
-     *  
-     */
-
-
-    /*
-     * Section 3.9.3 Variable Manipulation
-     */
-    public float InputValue;
-    public float MultiplyBy;
-    public float Result;
-
-    void Update()
+    using UnityEngine;
+    [UnityEngine.ExecuteInEditMode]
+    class Variables : UnityEngine.MonoBehaviour
     {
-        /*              math is done      */
-        /* ┌─────────────────┐①           */
-        /* │ result is  ┌────┴────┐       */
-        /* ② assigned ┌─┤operation├─┐     */
-        /* ↓          ↑ └─────────┘ ↑     */
-        Result = InputValue * MultiplyBy;
-    }
+        /*
+         * Section 3.9.2 Data
+         * 
+         * A Declaration is the announcement to
+         * Unity about your new variable.
+         *
+         * ┌──────┐┌────────────┐┌───────────┐
+         * │ Type ││ Identifier ││ Separator │
+         * └─┬────┘└────┬───────┘└────┬──────┘
+         *   └─────┐    └┐            │
+         *         │     │    ┌───────┘
+         *       ──┴── ──┴─── ┴ 
+         *       float myFloat;
+         *       └────┬───────┘
+         *        ┌───┘
+         *  ┌─────┴─────────────────┐
+         *  │ Declaration Statement │
+         *  └───────────────────────┘
+         *  
+         */
 
-    void MoreUpdates()
-    {
-        int i = 0;
-        System.Console.Write(i);
-        // i has been assigned 0
 
-        i = 1;
-        System.Console.Write(i);
-        // i reasssigned to 1
+        /*
+         * Section 3.9.3 Variable Manipulation
+         */
+        public float InputValue;
+        public float MultiplyBy;
+        public float Result;
 
-        //int i = 1;
-        // uncomment the line above to see the
-        // error.
+        void Update()
+        {
+            /*              math is done      */
+            /* ┌─────────────────┐①           */
+            /* │ result is  ┌────┴────┐       */
+            /* ② assigned ┌─┤operation├─┐     */
+            /* ↓          ↑ └─────────┘ ↑     */
+            Result = InputValue * MultiplyBy;
+        }
+
+        void MoreUpdates()
+        {
+            int i = 0;
+            System.Console.Write(i);
+            // i has been assigned 0
+
+            i = 1;
+            System.Console.Write(i);
+            // i reasssigned to 1
+
+            //int i = 1;
+            // uncomment the line above to see the
+            // error.
+        }
+
+        /*
+         * Section 3.10.2 Variable Assignment
+         */
+
+        private void Start()
+        {
+            {
+                /* int declares a new thing         */
+                /* assignment operator = assigns    */
+                /* the new int thing a value        */
+                /*        ┌───┐                     */
+                /*      ┌─┤ = ├──┐                  */
+                /*      ↓ └───┘  ↑                  */
+                int MyVariable = 7;
+                /*      ↓                           */
+                /*      └─────┐                     */
+                /*            ↓                     */
+                Debug.Log(MyVariable);
+                /* Debug.Log prints to the console  */
+                /* what is in it's argument list () */
+                // 7
+                /*   ┌───────┐ MyVariable           */
+                /*   ↓       ↑ gets a new value     */
+                MyVariable = 13;
+                Debug.Log(MyVariable);
+                // 13
+                /* the new value is printed         */
+
+                MyVariable = 3;
+                Debug.Log(MyVariable);
+                // 3
+                MyVariable = 73;
+                Debug.Log(MyVariable);
+                // 73
+            }
+                
+            {
+                int MyVariable = 7;
+                /* terms are               then the */
+                /* evaluated   ① ( 7 + 7 ) result is*/
+                /* first       ②=( 14 )    printed  */
+                Debug.Log(MyVariable + MyVariable);
+                // 14          ③ 14 appears in the console
+                /* this is functionally the same as */
+                Debug.Log(7 + 7);/* this            */
+                // 14
+            }
+
+            {
+                int MyVariable = 7;
+                int MyOtherVariable = 3;
+
+                /*  terms are    ( 7 * 3 )          */
+                Debug.Log(MyVariable * MyOtherVariable);
+                // 21
+                /* the same as   ( 7 * 3)           */
+                Debug.Log(MyVariable * 3);
+                // 21
+            }
+            {
+                int MyVariable = 7;
+                int MyOtherVariable = 3;
+                Debug.Log(MyVariable * MyOtherVariable);
+                // 21
+                /*   ┌──←=(3)┐ MyVariable           */
+                /*   ↓       ↑ gets a new value     */
+                MyVariable = MyOtherVariable;
+
+                /*  terms are    ( 3 * 3 )          */
+                Debug.Log(MyVariable * MyOtherVariable);
+                // 9
+
+            }
+        }
     }
 }
+
 
 /*
  * Section 3.9.3.1 Declaration Placement Examples
@@ -105,13 +185,13 @@ class Variables : UnityEngine.MonoBehaviour
  * { and before the closing curly brace }
  */
 
- /* 
-  * comment out the lower line to
-  * produce an error
-  */
+            /* 
+             * comment out the lower line to
+             * produce an error
+             */
 
-//int outterInt;
-class SomeClass
+            //int outterInt;
+        class SomeClass
 {
     int innerInt;
 
