1c0092b216b8df4e10a9aefdceff71ff863e29c5
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-12-01 17:40:31 -0800

    adding some headers.

---
+++ Chapters/Chapter2/Assets/GameEngines.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Chapter 2.3 Game Engines
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *             ┌──────────────────────────────────────┐  *
+ *             │ Art Assets         Node Based        │  *
+ *             │ Sounds and Music   Game Behaviour    │  *
+ *             │ UI construction                      │  *
+ *             ╞══════════════════════════════════════╡  *
+ *  high level │ C# Game Behaviour                    │  *
+ *      ↑      │ Implementation                       │  *
+ *             ╞══════════════════════════════════════╡  *               
+ *             │ 3rd Party Libraries and DLLs         │  *
+ *             │ Usually specific for each target OS  │  *
+ *             ╞══════════════════════════════════════╡  *
+ *             │ Unity 3D Engine C#                   │  *
+ *             │ Platform Independence                │  *
+ *             │ Layer                                │  *
+ *             ╞══════════════════════════════════════╡  *
+ *             │ Unity 3D Engine                      │  *
+ *             │ Math Rendering Effects               │  *
+ *             │ File Management                      │  *
+ *             │ Physics (All the hard stuff)         │  *
+ *             ╞══════════════════════════════════════╡  *
+ *             │ Operating System                     │  *
+ *             │ Windows/OsX/Android/iOS/Linux        │  *
+ *             │ etc...                               │  *
+ *             ╞══════════════════════════════════════╡  *
+ *             │ Hardware Software Interface          │  *
+ *             │ Drivers Specific Application         │  *
+ *             │ Programming Interfaces               │  *
+ *      ↑      ╞══════════════════════════════════════╡  *
+ *   Low Level │ Hardware Chips Controllers           │  *
+ *             └──────────────────────────────────────┘  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+class GameEngines
+{
+}
--- Chapters/Chapter2/Assets/LineNumbers.cs
+++ Chapters/Chapter2/Assets/LineNumbers.cs
@@ -1,5 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/*
+ * Chapter 2.2 Class Construction
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 using UnityEngine;
 
 public class LineNumbers : MonoBehaviour
