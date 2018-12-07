1a2b7f41bb720f321f71cd9a4c9e12e8e1bead93
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-12-07 03:10:21 -0800

    adding new assets for end of chapter 6 building a game chapter

--- SampleGame/Unity-SampleGame/Assets/SampleGame/Scripts/Chapter6/FloorGenerator.cs
+++ SampleGame/Unity-SampleGame/Assets/SampleGame/Scripts/Chapter6/FloorGenerator.cs
@@ -2,56 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
-public class FloorGenerator : MonoBehaviour
+namespace Chapter6
 {
-    // Trigger in the Inspector Panel to
-    // generate tiles
-    public bool GenerateTiles;
-    // Original prefab objects to make
-    // a tile floor from
-    public GameObject[] TileOriginals;
-    // a List of tiles to store if
-    // we want to make new tiles
-    public GameObject[] TileObjects;
-    // How many tiles to make
-    public int Width;
-    public int Depth;
-
-    void Update()
+    [ExecuteInEditMode]
+    public class FloorGenerator : MonoBehaviour
     {
-        if (GenerateTiles)
+        // Trigger in the Inspector Panel to
+        // generate tiles
+        public bool GenerateTiles;
+        // Original prefab objects to make
+        // a tile floor from
+        public GameObject[] TileOriginals;
+        // a List of tiles to store if
+        // we want to make new tiles
+        public GameObject[,] TileObjects;
+        // How many tiles to make
+        public int Width;
+        public int Depth;
+        private List<GameObject> TileObjectList;
+
+        void Update()
         {
-            GenerateTiles = false;
-
-            //clear any tiles that might already be there
-            if (TileObjects.Length > 0)
+            if (GenerateTiles)
             {
-                foreach (GameObject go in TileObjects)
+                GenerateTiles = false;
+
+                //clear any tiles that might already be there
+                foreach (GameObject tile in TileObjectList)
                 {
-                    DestroyImmediate(go);
+                    DestroyImmediate(tile);
                 }
-                TileObjects = null;
-            }
+                TileObjectList = new List<GameObject>();
 
-            // Make new tiles
-            List<GameObject> tiles = new List<GameObject>();
-            for (int x = 0; x < Width; x++)
-            {
-                for (int z = 0; z < Depth; z++)
+                TileObjects = new GameObject[Width, Depth];
+                for (int x = 0; x < Width; x++)
                 {
-                    int o = (z + x) % TileOriginals.Length;
-                    GameObject tile = GameObject.Instantiate(TileOriginals[o], transform);
-                    tile.transform.position = new Vector3()
+                    for (int z = 0; z < Depth; z++)
                     {
-                        x = x - (Width/2),
-                        z = z - (Depth/2)
-                    };
-                    tiles.Add(tile);
+                        int o = (z + x) % TileOriginals.Length;
+                        TileObjects[x, z] = GameObject.Instantiate(TileOriginals[o], transform);
+                        TileObjects[x, z].transform.position = new Vector3()
+                        {
+                            x = x - (Width / 2),
+                            z = z - (Depth / 2)
+                        };
+                        // keep track of what to delete when we
+                        // need to make a new set of tiles.
+                        TileObjectList.Add(TileObjects[x, z]);
+                    }
                 }
             }
-            TileObjects = new GameObject[tiles.Count];
-            tiles.CopyTo(TileObjects);
         }
     }
 }
---
+++ SampleGame/Unity-SampleGame/Assets/SampleGame/Scripts/Chapter6/RoomGenerator.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter6
+{
+    [ExecuteInEditMode]
+    public class RoomGenerator : MonoBehaviour
+    {
+        public bool GenerateRooms;
+        public GameObject WallObject;
+        public int RoomCount = 5;
+        // minimum is 5.
+        public int MaxRoomWidth = 7;
+        public int MaxRoomDepth = 7;
+        private List<GameObject> WallObjectList;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (GenerateRooms)
+            {
+                GenerateRooms = false;
+
+                // clear any walls
+                if (WallObjectList != null)
+                {
+                    foreach (GameObject wall in WallObjectList)
+                    {
+                        DestroyImmediate(wall);
+                    }
+                }
+
+                FloorGenerator generator = GetComponent<FloorGenerator>();
+                /*      14-6 = 8                                        */
+                /*     ₓ┌──────┐ₓₓₓₓₓ                                   */
+                /*     12345678901234                                   */
+                /*   ₓ1╔════════════╗  Width  = 14                      */
+                /*   ┌2║①━━━┓▪▪▪▫▫▫▫║  Height = 13                      */
+                /*   │3║┃◦◦◦┃▪▪▪▫▫▫▫║  ① is the first valid point       */
+                /*   │4║┃◦◦◦┃▪▪▪▫▫▫▫║    to start a room.               */
+                /* 13│5║┃◦◦◦┃▪▪▪▫▫▫▫║                                   */
+                /* -6│6║┗━━━┛▪▪▪▫▫▫▫║                                   */
+                /* ──│7║▪▪▪▪▪▪▪▪▫▫▫▫║  if MaxRoomWidth/Depth = 5        */
+                /*  7└8║▪▪▪▪▪▪▪②━━━┓║  ② Last point in the area that    */
+                /*   ₓ9║▫▫▫▫▫▫▫┃◦◦◦┃║    can fit a 5x5 room             */
+                /*   ₓ0║▫▫▫▫▫▫▫┃◦◦◦┃║                                   */
+                /*   ₓ1║▫▫▫▫▫▫▫┃◦◦◦┃║                                   */
+                /*   ₓ2║▫▫▫▫▫▫▫┗━━━┛║                                   */
+                /*   ₓ3╚════════════╝                                   */
+                /*                                                      */
+                /* if a room is going to be a                           */
+                /* maximum size of 5x5 then the                         */
+                /* last point we can select in                          */
+                /* each row is 5 away from the                          */
+                /* edge.                                                */
+                
+                WallObjectList = new List<GameObject>();
+                // remember what points were used.
+                int maxW = 1 + generator.Width - MaxRoomWidth;
+                int maxD = 1 + generator.Depth - MaxRoomDepth;
+                int[,] usedPoints = new int[maxW, maxD];
+
+                for (int i = 0; i < RoomCount; i++)
+                {
+                    // a point somewhere in the safe zone
+                    int w = Random.Range(1, maxW);
+                    int d = Random.Range(1, maxD);
+                    MakeRoom(w, d);
+                }
+
+                void MakeRoom(int x, int z)
+                {
+                    int sizeX = x + Random.Range(5, MaxRoomWidth);
+                    int sizeZ = z + Random.Range(5, MaxRoomDepth);
+                    int width = sizeX - x;
+                    int depth = sizeZ - z;
+
+                    // make sure sizes are odd not even
+                    if ((width & 1) == 0 && width > 5)
+                    {
+                        sizeX--;
+                    }
+
+                    if ((depth & 1) == 0 && depth > 5)
+                    {
+                        sizeZ--;
+                    }
+
+                    for (int sX = x; sX < sizeX; sX++)
+                    {
+                        for (int sZ = z; sZ < sizeZ; sZ++)
+                        {
+                            // make north south walls
+                            if (sX == x || sX == sizeX - 1)
+                            {
+                                GameObject wall = Instantiate(WallObject, transform);
+                                WallObjectList.Add(wall);
+                                wall.transform.position = new Vector3()
+                                {
+                                    x = sX - (generator.Width / 2),
+                                    z = sZ - (generator.Depth / 2)
+                                };
+                            }
+                            else if (sZ == z || sZ == sizeZ - 1)
+                            {
+                                GameObject wall = Instantiate(WallObject, transform);
+                                WallObjectList.Add(wall);
+                                wall.transform.position = new Vector3()
+                                {
+                                    x = sX - (generator.Width / 2),
+                                    z = sZ - (generator.Depth / 2)
+                                };
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
