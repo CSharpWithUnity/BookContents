0d1494aacff2281dce2a9ddf8bf3f189c5ae653c
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-12-06 20:24:09 -0800

    adding new assets and code for chapter 6

---
+++ SampleGame/Unity-SampleGame/Assets/SampleGame/Scripts/Chapter6/ChaseCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter6
+{
+    public class ChaseCamera : MonoBehaviour
+    {
+        private GameObject _Target;
+        private GameObject Target
+        {
+            get
+            {
+                if (_Target == null)
+                {
+                    // find the player, keep
+                    // a reference to it.
+                    _Target = Chapter5.Player.ThePlayer;
+                }
+                return _Target;
+            }
+        }
+
+        private Vector3 Offset;
+        private void Update()
+        {
+            if (Target == null)
+                return;
+            // Calculate the difference between previous position
+            // and the new position.
+            Vector3 diff = Target.transform.position - Offset;
+            // Move the camera that difference.
+            transform.position += diff;
+            // update the Offset value.
+            Offset = Target.transform.position;
+        }
+    }
+}
---
+++ SampleGame/Unity-SampleGame/Assets/SampleGame/Scripts/Chapter6/FloorGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FloorGenerator : MonoBehaviour
+{
+    // Trigger in the Inspector Panel to
+    // generate tiles
+    public bool GenerateTiles;
+    // Original prefab objects to make
+    // a tile floor from
+    public GameObject[] TileOriginals;
+    // a List of tiles to store if
+    // we want to make new tiles
+    public GameObject[] TileObjects;
+    // How many tiles to make
+    public int Width;
+    public int Depth;
+
+    void Update()
+    {
+        if (GenerateTiles)
+        {
+            GenerateTiles = false;
+
+            //clear any tiles that might already be there
+            if (TileObjects.Length > 0)
+            {
+                foreach (GameObject go in TileObjects)
+                {
+                    DestroyImmediate(go);
+                }
+                TileObjects = null;
+            }
+
+            // Make new tiles
+            List<GameObject> tiles = new List<GameObject>();
+            for (int x = 0; x < Width; x++)
+            {
+                for (int z = 0; z < Depth; z++)
+                {
+                    int o = (z + x) % TileOriginals.Length;
+                    GameObject tile = GameObject.Instantiate(TileOriginals[o], transform);
+                    tile.transform.position = new Vector3()
+                    {
+                        x = x - (Width/2),
+                        z = z - (Depth/2)
+                    };
+                    tiles.Add(tile);
+                }
+            }
+            TileObjects = new GameObject[tiles.Count];
+            tiles.CopyTo(TileObjects);
+        }
+    }
+}
