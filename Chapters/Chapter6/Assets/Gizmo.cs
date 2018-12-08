ae28afd9194450320deb853ab2444f4643dadb20
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-12-08 01:54:32 -0800

    adding some new stiff for Gizmos and stuff for chapter 6.15

---
+++ Chapters/Chapter6/Assets/Gizmo.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Chapter 6.14 Type Casting Again
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter6_15
+{
+    public class Gizmo : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        private void OnDrawGizmos()
+        {
+            /*
+             * Chapter 6.15.3.1 Gizmos A Basic Example
+             * uncomment the line below to see the cuboid
+             * gizmo drawn at the origin.
+             */
+
+            //Gizmos.DrawCube(Vector3.zero, new Vector3(1, 2, 3));
+            
+            /*
+             * Chapter 6.15.3.2 DrawRay
+             */
+            Ray ray = new Ray();
+            ray.origin = Vector3.zero;
+            ray.direction = Vector3.up;
+            Gizmos.DrawRay(ray);
+
+            /*
+             * Chapter 6.15.4 Using Gizmos
+             */
+
+            Gizmos.DrawLine(transform.position, Vector3.zero);
+
+        }
+    }
+}
