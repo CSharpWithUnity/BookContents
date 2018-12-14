d634a86c5a27ec4c2cfe6133e53d634bf75382e9
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-12-13 22:04:42 -0800

    adding some work for chapter 8.2

---
+++ Chapters/Chapter8/Assets/Review.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Chapter 8.2 Review
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+namespace Chapter8_2
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Review : MonoBehaviour
+    {
+        void UseHasProperties()
+        {
+            CanIHasProperties hasProperties = new CanIHasProperties();
+            hasProperties.IHasFloat = 5f;
+            Debug.Log(hasProperties.IHasFloat);
+            // 5
+            Debug.Log(hasProperties.IHasHalfFloat);
+            // 2.5
+            hasProperties.IHasHalfFloat = 9f;
+            Debug.Log(hasProperties.IHasFloat);
+            // 18
+        }
+    }
+
+    class CanIHasProperties
+    {
+        protected float hasFloat;
+        public float IHasFloat
+        {
+            get { return hasFloat; }
+            set { hasFloat = value; }
+        }
+        public float IHasHalfFloat
+        {
+            get { return hasFloat * 0.5f; }
+            set { hasFloat = value * 2; }
+        }
+    }
+
+}
