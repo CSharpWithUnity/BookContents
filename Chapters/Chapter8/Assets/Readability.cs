064eac7957c3b0f3a80974b05cc7557986542736
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-12-13 22:37:12 -0800

    adding more namespaces

---
+++ Chapters/Chapter8/Assets/Readability.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Chapter 8.3 Readability
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+namespace Chapter8_3
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Readability : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
