30d7302ada23f3a00c52a6d012f09a1bc5a1ab70
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-12-14 01:33:58 -0800

    added git stuff

---
+++ Chapters/Chapter8/Assets/GitRevisited.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter8_4
+{
+    public class GitRevisited : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Player1 Start");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
