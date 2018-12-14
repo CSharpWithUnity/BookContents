d8fdf7100212c434b42128732be24eed1e36623b
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-12-13 23:46:55 -0800

    adding readability content

--- Chapters/Chapter8/Assets/Readability.cs
+++ Chapters/Chapter8/Assets/Readability.cs
@@ -15,10 +15,85 @@ namespace Chapter8_3
 
     public class Readability : MonoBehaviour
     {
+        /*
+         * Section 8.3.1 ?: Ternary Conditional Operator
+         */
+        void UseTernaryConditionalOperator()
+        {
+            {
+                string result;
+                bool condition = true;
+                if (condition)
+                {
+                    result = "True";
+                }
+                else
+                {
+                    result = "False";
+                }
+                Debug.Log("Result of condition = " + result);
+                // Result of condition = True
+            }
+
+            {
+                bool condition = true;
+                /*       └──────────┐                       */
+                /*                  ↓                       */
+                string result = condition ? "True" : "False";
+                /*       ↑                 ①──┬───   ②──┬──         */
+                /*       │            ┌───────┴─────────┴──────────┐*/
+                /*       │            │first statement returned    │*/
+                /*       └───────────←┤if condition is true else   │*/
+                /*                    │second statement is returned│*/
+                /*                    └────────────────────────────┘*/
+                Debug.Log("Result of condition = " + result);
+                // Result of condition = True
+            }
+
+            {
+                int greaterThan(int a, int b)
+                {
+                    return a > b ? a : b;
+                }
+                Debug.Log("Greater:" + greaterThan(5, 7));
+                // Greater:7
+            }
+
+            {
+                int greaterThan(int a, int b, int c)
+                {
+                    return (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+                }
+                Debug.Log("Greater:" + greaterThan(5, 7, 11));
+                // Greater:11
+            }
+
+            {
+                int greaterThan(int a, int b, int c)
+                {
+                    return a > b ? a > c ? a : c : b > c ? b : c;
+                }
+                Debug.Log("Greater:" + greaterThan(5, 7, 11));
+                // Greater:11
+            }
+        }
+
+        /*
+         * 8.3.2 If
+         */
+        void UseIf()
+        {
+
+        }
+
+
         // Start is called before the first frame update
         void Start()
         {
-
+            /*
+             * Section 8.3.1 ?: Ternary Conditional Operator
+             */
+            UseTernaryConditionalOperator();
         }
 
         // Update is called once per frame
