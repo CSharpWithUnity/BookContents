ac513f65f83ade15c1e9e2a02689cb3c20836a92
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-12-27 02:20:35 -0800

    updates to names

---
+++ Chapters/Chapter8/Assets/GameCo/Scripts/ZombieGame/Damage.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Chapter 8.10 Architecture and Organization
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+namespace GameCo.ZombieGame
+{
+    public class Damage
+    {
+        public DamageData Info;
+    }
+
+    public class DamageData
+    {
+        public DamageTypes DamageType;
+        public int DamageAmount;
+    }
+
+    public enum DamageTypes
+    {
+        Projectile  = 0,
+        Poison      = 1,
+        Fire        = 2,
+        Explisove   = 4,
+        Cutting     = 8,
+        Dimensional = 16,
+        Freezing    = 32,
+        Crushing    = 64,
+        Falling     = 128
+    }
+}
