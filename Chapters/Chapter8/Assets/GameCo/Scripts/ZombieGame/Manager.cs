c7d69bdcf2041e00b96df345c211e87f1db0130b
BUGGY\Alex Okita <alex.okita@gmail.com>
2018-12-27 20:55:45 -0800

    updating Manager

---
+++ Chapters/Chapter8/Assets/GameCo/Scripts/ZombieGame/Manager.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Chapter 8.11 Design Patterns
+ *
+ * Copyright © 2018 Alex Okita
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+using UnityEngine;
+namespace GameCo.ZombieGame
+{
+    public class Manager : MonoBehaviour
+    {
+        /*
+         * Singleton Pattern
+         */
+        public static Manager ZombieManager;
+        void Awake()
+        {
+            if(ZombieManager == null)
+            {
+                ZombieManager = this;
+            }
+        }
+    }
+}
