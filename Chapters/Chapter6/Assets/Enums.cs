50e4e1e76cb3e453467f9a1596fcd1b8ffb74fff
BUGGY\Alex Okita <alex.okita@gmail.com>
2019-01-05 22:39:29 -0800

    more ch6 cleanup

--- Chapters/Chapter6/Assets/Enums.cs
+++ Chapters/Chapter6/Assets/Enums.cs
@@ -1,79 +1,84 @@
-﻿/*
- * Chapter 6.6 Enums
- *
- * Copyright © 2018 Alex Okita
- *
- * This software may be modified and distributed under the terms
- * of the MIT license.  See the LICENSE file for details.
- */
-
-using UnityEngine;
-
-public class Enums : MonoBehaviour
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Chapter 6.6 Enums                                                 *
+ *                                                                   *
+ * Copyright © 2018 Alex Okita                                       *
+ *                                                                   *
+ * This software may be modified and distributed under the terms     *
+ * of the MIT license.  See the LICENSE file for details.            *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+namespace Chapter6_6
 {
-    /*
-     * Section 6.6.1 Using Enums
-     */
-    public PrimitiveType MyPrimitiveType;
-    /*
-     * Right Click on PrimitiveType to go to the
-     * definition and see what the enum looks like.
-     */
+    using UnityEngine;
+    public class Enums : MonoBehaviour
+    {
+        #region Chapter 6.6.1 Using Enums
+        /* * * * * * * * * * * * * * * *
+         * Section 6.6.1 Using Enums   *
+         * * * * * * * * * * * * * * * */
 
-    enum YesOrNo
-    {
-        yes,
-        no
-    };
-    YesOrNo yesOrNoEnum;
+        public PrimitiveType MyPrimitiveType;
+        /*
+         * Right Click on PrimitiveType to go to the
+         * definition and see what the enum looks like.
+         */
 
-    /*
-     * Section 6.6.1 Continued...
-     */
-    public enum MyColors
-    {
-        red,
-        blue,
-        green
-    };
-    // also valid
-    // public enum MyColors {red, blue, green };
-    public MyColors MyColor;
-    GameObject gameObject;
-    private void Start()
-    {
+        enum YesOrNo
         {
-            /*
-             * Section 6.6.1 Using Enums
-             */
+            yes,
+            no
+        };
+        YesOrNo yesOrNoEnum;
+
+        /* * * * * * * * * * * * * * * *
+         * Section 6.6.1 Continued...  *
+         * * * * * * * * * * * * * * * */
+        public enum MyColors
+        {
+            red,
+            blue,
+            green
+        };
+
+        // also valid
+        // public enum MyColors {red, blue, green };
+        public MyColors MyColor;
+        GameObject gameObject;
+
+        /* * * * * * * * * * * * * * * *
+         * Section 6.6.1 Continued...  *
+         * * * * * * * * * * * * * * * */
+        void UseEnums()
+        {
             if (yesOrNoEnum == YesOrNo.yes)
             {
                 Debug.Log("yes");
             }
+
             bool yesOrNoBool = true;
             if (yesOrNoBool == true)
             {
                 Debug.Log("yes");
             }
+
             int primitiveInt = 0;
-            /* 0 = Sphere,
-             * 1 = Capsule,
-             * 2 = Cylinder,
-             * 3 = Cube,
-             * 4 = Plane,
-             * 5 = Quad
-             */
+            /* 0 = Sphere,   *
+             * 1 = Capsule,  *
+             * 2 = Cylinder, *
+             * 3 = Cube,     *
+             * 4 = Plane,    *
+             * 5 = Quad      */
+
             if (primitiveInt == 0)
             {
                 Debug.Log("Sphere");
             }
-
         }
 
+        /* * * * * * * * * * * * * * * *
+         * Section 6.6.1 Continued...  *
+         * * * * * * * * * * * * * * * */
+        void UseMoreEnums()
         {
-            /*
-             * Section 6.6.1 Continued...
-             */
             gameObject = GameObject.CreatePrimitive(MyPrimitiveType);
             MeshRenderer meshRenderer = gameObject.GetComponent(typeof(MeshRenderer)) as MeshRenderer;
             Material material = meshRenderer.material;
@@ -89,6 +94,21 @@ public class Enums : MonoBehaviour
             {
                 material.color = Color.green;
             }
+        }
+        #endregion
+
+
+        private void Start()
+        {
+            /* * * * * * * * * * * * * * * *
+             * Section 6.6.1 Using Enums   *
+             * * * * * * * * * * * * * * * */
+            UseEnums();
+
+            /* * * * * * * * * * * * * * * *
+             * Section 6.6.1 Continued...  *
+             * * * * * * * * * * * * * * * */
+            UseMoreEnums();
         }
     }
 }
