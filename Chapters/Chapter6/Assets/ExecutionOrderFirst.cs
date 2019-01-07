9136a7ec7d72a36752bc71812ab0101c3e81352a
BUGGY\Alex Okita <alex.okita@gmail.com>
2019-01-06 21:05:49 -0800

    more comment cleanup

--- Chapters/Chapter6/Assets/ExecutionOrderFirst.cs
+++ Chapters/Chapter6/Assets/ExecutionOrderFirst.cs
@@ -1,62 +1,63 @@
-﻿/*
- * Chapter 6.12.1 Execution Order
- *
- * Copyright © 2018 Alex Okita
- *
- * This software may be modified and distributed under the terms
- * of the MIT license.  See the LICENSE file for details.
- */
-
-using UnityEngine;
-
-public class ExecutionOrderFirst : MonoBehaviour
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Chapter 6.12.1 Execution Order                                    *
+ *                                                                   *
+ * Copyright © 2018 Alex Okita                                       *
+ *                                                                   *
+ * This software may be modified and distributed under the terms     *
+ * of the MIT license.  See the LICENSE file for details.            *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+namespace Chapter6_12
 {
-
-    void Awake()
+    using UnityEngine;
+    public class ExecutionOrderFirst : MonoBehaviour
     {
+
+        void Awake()
         {
-            Debug.Log("First Awake");
+            {
+                Debug.Log("First Awake");
+            }
+            {
+                Debug.Log("First Awake Start");
+                this.gameObject.AddComponent(typeof(ExecutionOrderSecond));
+                Debug.Log("First Awake Done");
+            }
         }
+
+        void OnEnable()
         {
-            Debug.Log("First Awake Start");
-            this.gameObject.AddComponent(typeof(ExecutionOrderSecond));
-            Debug.Log("First Awake Done");
+            Debug.Log("First OnEnable");
         }
-    }
 
-    void OnEnable()
-    {
-        Debug.Log("First OnEnable");
-    }
+        void Start()
+        {
+            Debug.Log("First Start");
+        }
 
-    void Start()
-    {
-        Debug.Log("First Start");
-    }
+        void FixedUpdate()
+        {
+            Debug.Log("First FixedUpdate");
+        }
 
-    void FixedUpdate()
-    {
-        Debug.Log("First FixedUpdate");
-    }
+        void Update()
+        {
+            Debug.Log("First Update");
+        }
 
-    void Update()
-    {
-        Debug.Log("First Update");
-    }
+        private void LateUpdate()
+        {
+            Debug.Log("First LateUpdate");
+            Destroy(this);
+        }
 
-    private void LateUpdate()
-    {
-        Debug.Log("First LateUpdate");
-        Destroy(this);
-    }
+        private void OnDisable()
+        {
+            Debug.Log("First OnDisable");
+        }
 
-    private void OnDisable()
-    {
-        Debug.Log("First OnDisable");
-    }
-
-    private void OnDestroy()
-    {
-        Debug.Log("First OnDistroy");
+        private void OnDestroy()
+        {
+            Debug.Log("First OnDistroy");
+        }
     }
 }
--- Chapters/Chapter6/Assets/ExecutionOrderThird.cs
+++ Chapters/Chapter6/Assets/ExecutionOrderThird.cs
@@ -1,55 +1,55 @@
-﻿/*
- * Chapter 6.12.1 Execution Order
- *
- * Copyright © 2018 Alex Okita
- *
- * This software may be modified and distributed under the terms
- * of the MIT license.  See the LICENSE file for details.
- */
-
-using UnityEngine;
-
-public class ExecutionOrderThird : MonoBehaviour
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Chapter 6.12.1 Execution Order                                    *
+ *                                                                   *
+ * Copyright © 2018 Alex Okita                                       *
+ *                                                                   *
+ * This software may be modified and distributed under the terms     *
+ * of the MIT license.  See the LICENSE file for details.            *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+namespace Chapter6_12
 {
-
-    void Awake()
+    using UnityEngine;
+    public class ExecutionOrderThird : MonoBehaviour
     {
-        Debug.Log("Third Awake");
-    }
+        void Awake()
+        {
+            Debug.Log("Third Awake");
+        }
 
-    void OnEnable()
-    {
-        Debug.Log("Third OnEnable");
-    }
+        void OnEnable()
+        {
+            Debug.Log("Third OnEnable");
+        }
 
-    void Start()
-    {
-        Debug.Log("Third Start");
-    }
+        void Start()
+        {
+            Debug.Log("Third Start");
+        }
 
-    void FixedUpdate()
-    {
-        Debug.Log("Third FixedUpdate");
-    }
+        void FixedUpdate()
+        {
+            Debug.Log("Third FixedUpdate");
+        }
 
-    void Update()
-    {
-        Debug.Log("Third Update");
-    }
+        void Update()
+        {
+            Debug.Log("Third Update");
+        }
 
-    private void LateUpdate()
-    {
-        Debug.Log("Third LateUpdate");
-        Destroy(this);
-    }
+        private void LateUpdate()
+        {
+            Debug.Log("Third LateUpdate");
+            Destroy(this);
+        }
 
-    private void OnDisable()
-    {
-        Debug.Log("Third OnDisable");
-    }
+        private void OnDisable()
+        {
+            Debug.Log("Third OnDisable");
+        }
 
-    private void OnDestroy()
-    {
-        Debug.Log("Third OnDistroy");
+        private void OnDestroy()
+        {
+            Debug.Log("Third OnDistroy");
+        }
     }
 }
